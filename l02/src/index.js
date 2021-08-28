e260795c16f3d30582ab6d8899a26277843fe8a6
zhamkovv <zhamkov@gmail.com>
2021-08-28 10:51:23 +0300

    Create react app

---
+++ l02/src/index.js
@@ -0,0 +1,10 @@
+// eslint-disable-next-line
+import React from 'react'; 
+import ReactDOM from 'react-dom';
+import './index.css';
+import element from './App.element';
+
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
